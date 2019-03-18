73bc7da0443913896a1424af12c9c379211f28a0
Danil Zaviryukha <logolevel@gmail.com>
2019-03-18 02:32:45 +0200

    Styles for Composer, Post and Feed. Create a component StausBar

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -4,11 +4,12 @@ import React, { Component } from 'react';
 //Instruments
 import avatar from 'theme/assets/lisa.png';
 import moment from 'moment';
+import Styles from './styles.m.css';
 
 export default class Post extends Component {
     render() {
         return (
-            <section>
+            <section className = { Styles.post }>
                 <img src = { avatar } />
                 <a>Lisa Sympson</a>
                 <time>{ moment().format('MMM D h:mm:ss a') }</time>
