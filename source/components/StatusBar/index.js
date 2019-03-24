538c99d7adbae4df98b6f3e0bc87ecc0b29eb246
Danil Zaviryukha <logolevel@gmail.com>
2019-03-24 22:59:16 +0200

    Attach components to context. Components: StatusBar, Composer, Post

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,25 +1,26 @@
 //Core
 import React, { Component } from 'react';
 
+//Components
+import { Consumer } from 'components/HOC/withProfile';
+
 //Instruments
 import Styles from './styles.m.css';
 
 export default class StatusBar extends Component {
     render() {
-        const {
-            avatar,
-            currentUserFirstName,
-            currentUserLastName
-        } = this.props;
-
         return (
-            <section className = { Styles.statusBar }>
-                <button>
-                    <img src = { avatar } />
-                    <span>{currentUserFirstName}</span>
-                    <span>{currentUserLastName}</span>
-                </button>
-            </section>
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.statusBar }>
+                        <button>
+                            <img src = { context.avatar } />
+                            <span>{ context.currentUserFirstName }</span>
+                            <span>{ context.currentUserLastName }</span>
+                        </button>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
