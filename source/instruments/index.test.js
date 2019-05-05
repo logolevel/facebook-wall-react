fba208ac525eb17b0ed31dcd71a956041f0e6298
Danil Zaviryukha <logolevel@gmail.com>
2019-05-06 00:16:15 +0300

    Add some tests for Instruments

--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -1,5 +1,5 @@
 //Core
-import { sum, delay, getUniqueID } from './';
+import { sum, delay, getUniqueID, getFullApiUrl } from './';
 
 describe('instruments:', () => {
     test('sum function should be a function', () => {
@@ -23,16 +23,32 @@ describe('instruments:', () => {
         await expect(delay()).resolves.toBeUndefined();
     });
 
-    test('getUniqueID function should be a function', async () => {
+    test('getUniqueID function should be a function', () => {
         expect(getUniqueID).toBeInstanceOf(Function);
     });
 
-    test('getUniqueID function should throw, when called with not-number argument', async () => {
+    test('getUniqueID function should throw, when called with not-string argument', async () => {
         expect(() => getUniqueID('hello')).toThrow();
     });
 
     test('getUniqueID function should produce a string of a desired given length', async () => {
         expect(typeof getUniqueID()).toBe('string');
         expect(getUniqueID(5)).toHaveLength(5);
+    });
+
+    test('getFullApiUrl function should be a function', () => {
+        expect(getFullApiUrl).toBeInstanceOf(Function);
+    });
+
+    test('getFullApiUrl function should throw, when called with not-string type as second argument', async () => {
+        expect(() => getFullApiUrl('hello', 2)).toThrow();
+    });
+
+    test('getFullApiUrl function should throw, when called with not-string type as first argument', async () => {
+        expect(() => getFullApiUrl(1, 'hello')).toThrow();
+    });
+
+    test('getFullApiUrl function should return right result', () => {
+        expect(getFullApiUrl('api-is-string', 'id-is-string')).toBe('api-is-string/id-is-string');
     });
 });
